137e1a6e4c13446c5d05e969bff49f7c13b0c721
alex <alex.med.kh@gmail.com>
2016-02-21 21:13:08 +0200

    feat(fm) update Song model

--- src/Song.php
+++ src/Song.php
@@ -36,4 +36,44 @@ class Song
         $resolver->setAllowedTypes(self::OPT_FILE, 'string');
         $resolver->setAllowedTypes(self::OPT_BITRATE, 'int');
     }
+
+    /**
+     * @return int
+     */
+    public function bitrate()
+    {
+        return $this->options[self::OPT_BITRATE];
+    }
+
+    /**
+     * @return float
+     */
+    public function frameSize()
+    {
+        return floor(144000.0 * $this->bitrate() / 44100);
+    }
+
+    /**
+     * @return float
+     */
+    public function playSpeed()
+    {
+        return 1000.0 * $this->bitrate() / 8.0;
+    }
+
+    /**
+     * @return string
+     */
+    public function syncFilePath()
+    {
+        return $this->file()->getRealPath() . '.txt';
+    }
+
+    /**
+     * @return SplFileObject
+     */
+    public function file()
+    {
+        return $this->options[self::OPT_FILE];
+    }
 }
