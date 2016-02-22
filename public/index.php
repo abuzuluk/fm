81a91e71b0edb3797fbfbce22c4fad0fa63267b4
alex <alex.med.kh@gmail.com>
2016-02-22 02:20:52 +0200

    feat(timezone) detect song stream depeneding of timezone

--- public/index.php
+++ public/index.php
@@ -5,11 +5,14 @@ require __DIR__ . '/../vendor/autoload.php';
 error_reporting(-1);
 set_time_limit(0);
 
-if (microtime(true) % 2) {
-    $song = new \Fm\Song(['bitrate' => 128, 'file' => __DIR__ . '/../mp3/128.mp3']);
-} else {
-    $song = new \Fm\Song(['bitrate' => 320, 'file' => __DIR__ . '/../mp3/320.mp3']);
+$tz = isset($_GET['tz']) ? $_GET['tz'] : 'UTC';
+if (!in_array($tz, DateTimeZone::listIdentifiers(), true)) {
+    $tz = 'UTC';
 }
+
+$now = new DateTime(null, new DateTimeZone($tz));
+$schedule = \Fm\Schedule::fromYaml(__DIR__ . '/mp3/schedule.yaml');
+$song = $schedule->search($now);
 
 $player = new \Fm\Player();
 $player->play($song, getenv('DEBUG'));
